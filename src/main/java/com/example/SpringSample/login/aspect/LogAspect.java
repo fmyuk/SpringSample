5dd13343948f850a531c433523a6442db9c5dbfb
fumi uk <dcfumiya.238@gmail.com>
2019-09-13 05:55:11 +0900

    change method

--- src/main/java/com/example/SpringSample/login/aspect/LogAspect.java
+++ src/main/java/com/example/SpringSample/login/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.SpringSample.login.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,13 +11,17 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LogAspect {
-    @Before("execution( * com.example.SpringSample.login.controller.LoginController.getLogin(..))")
-    public void startLog(JoinPoint jp) {
+    @Around("execution( * com.example.SpringSample.login.controller.LoginController.getLogin(..))")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
         System.out.println("メソッド開始： " + jp.getSignature());
-    }
-
-    @After("execution( * com.example.SpringSample.login.controller.LoginController.getLogin(..))")
-    public void endLog(JoinPoint jp) {
-        System.out.println("メソッド終了： " + jp.getSignature());
+        try {
+            Object result = jp.proceed();
+            System.out.println("メソッド終了： " + jp.getSignature());
+            return result;
+        } catch(Exception e) {
+            System.out.println("メソッド終了： " + jp.getSignature());
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
