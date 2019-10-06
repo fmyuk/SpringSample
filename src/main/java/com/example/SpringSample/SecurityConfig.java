611b2557dcd9fdc4e1918c5935c2f394c9f1c250
fumi uk <dcfumiya.238@gmail.com>
2019-10-07 07:03:03 +0900

    file changes

---
+++ src/main/java/com/example/SpringSample/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.SpringSample;
+
+public class SecurityConfig {
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,6 @@ VALUES(1, 'mizuho', 100);
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
 VALUES('mizuho@xxx.co.jp',
 'password', 'みずほFG', '1919-01-01', 100, false, 'ROLE_ADMIN');
+
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('ukai@xxx.co.jp', 'password', '鵜飼史也', '1994-09-11', 25, false, 'ROLE_GENERAL');
